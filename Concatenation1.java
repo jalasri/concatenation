98297d31ab95d37f71a67ce5f07a637641fde0f3
jalasri <91937124+jalasri@users.noreply.github.com>
2021-12-02 10:49:25 +0530

    Add files via upload

---
+++ Concatenation1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Hi
+ */
+public class Concatenation1 {
+    public static void main(String args[])
+    {
+            String s ="abdul"+"kalam";
+    System.out.println(s);
+}
+}    
